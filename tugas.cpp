4e2f880ce05c4cad1839fa6bb71ef2b4c2f063c6
MarsHallSec <47811548+MarsHallGans@users.noreply.github.com>
2020-02-19 08:40:53 +0000

    Add files via upload

---
+++ tugas.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int a,b;
+	bool hasil,hasill;
+
+	cout <<"============= Program Operator =============\n";
+	cout <<"#  Program operator ini untuk latihan PTS  #\n";
+	cout <<"# Program ini dibuat oleh Muhammad Faisyal #\n";
+	cout <<"============================================\n";
+
+	cout <<"Input A : "; cin >> a;
+	cout <<"Input B : "; cin >> b;
+
+	hasil = a > b;
+	hasill = a < b;
+
+	cout <<"A > B : " << hasil<<endl;
+	cout <<"A > B : " << hasil<<endl;
+
+	cout <<"A < B : " << hasill<<endl;
+	cout <<"A < B : " << hasill<<endl;
+
+	return 0;
+}
